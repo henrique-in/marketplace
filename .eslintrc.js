67f1a1d02ecdbe25d1e988aebac08ac9363fd402
Henrique <henrique.fsa97@hotmail.com>
2022-05-14 12:39:40 -0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    'env': {
+        node: true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error'
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    },
+    settings: {
+        react: {version: 'detect'},
+        'import/resolver': {
+            typescript: {},
+            'babel-plugin-root-import': {
+                rootPathPrefix: '~',
+                rootPathSuffix: 'src',
+            },
+        },
+    },
+}
